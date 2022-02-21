081217ea25d1687b6ee07da94dcc120f615a0eee
naomo123 <naomoiglesias@gmail.com>
2022-02-21 10:33:45 -0600

    cambio de idioma

--- modal/edit_delete_modal.php
+++ modal/edit_delete_modal.php
@@ -5,7 +5,7 @@
     			<div class="modal-header">
     				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
     				<center>
-    					<h4 class="modal-title" id="myModalLabel">Edit Member</h4>
+    					<h4 class="modal-title" id="myModalLabel">Edit Product</h4>
     				</center>
     			</div>
     			<div class="modal-body">
